238da47589eb5d29fcbe45687085267e58911612
Rupesh Jha <rupeshjha7796@gmail.com>
2025-09-09 18:21:54 +0530

    Update myFullPage.js

--- myFullPage.js
+++ myFullPage.js
@@ -1,88 +1,44 @@
-/*!
- * myFullPage.js v1.0.0
- * Simple fullpage scrolling sections (free alternative to fullPage.js)
- * Author: Your Name
- * License: MIT
- */
-
 class MyFullPage {
-  constructor(selector, options = {}) {
+  constructor(options) {
     // Default options
     this.options = Object.assign({
-      speed: 800,
-      loop: false,
-      keyboard: true,
-      onLeave: null,
-      afterLoad: null
+      sectionSelector: ".section",
+      scrollSpeed: 700
     }, options);
 
-    // Container and sections
-    this.container = document.querySelector(selector);
-    if (!this.container) throw new Error("Container not found!");
+    // Get sections
+    this.sections = document.querySelectorAll(this.options.sectionSelector);
+    this.currentSection = 0;
+    this.isScrolling = false;
 
-    this.sections = this.container.querySelectorAll("section");
-    this.index = 0;
-    this.isAnimating = false;
+    // Set initial view
+    this.scrollToSection(this.currentSection);
 
-    // Initial styles
-    this.container.style.transition = `transform ${this.options.speed}ms ease-in-out`;
-
-    // Bind events
-    this.bindEvents();
+    // Add scroll event
+    window.addEventListener("wheel", (e) => this.handleScroll(e));
   }
 
-  bindEvents() {
-    // Scroll with mouse
-    window.addEventListener("wheel", (e) => {
-      if (this.isAnimating) return;
-      if (e.deltaY > 0) this.next();
-      else this.prev();
-    }, { passive: true });
+  handleScroll(e) {
+    if (this.isScrolling) return;
 
-    // Keyboard support
-    if (this.options.keyboard) {
-      window.addEventListener("keydown", (e) => {
-        if (this.isAnimating) return;
-        if (e.key === "ArrowDown") this.next();
-        if (e.key === "ArrowUp") this.prev();
-      });
+    if (e.deltaY > 0 && this.currentSection < this.sections.length - 1) {
+      this.currentSection++;
+    } else if (e.deltaY < 0 && this.currentSection > 0) {
+      this.currentSection--;
     }
+
+    this.scrollToSection(this.currentSection);
   }
 
-  goTo(index) {
-    if (index < 0) {
-      if (this.options.loop) index = this.sections.length - 1;
-      else return;
-    }
-    if (index >= this.sections.length) {
-      if (this.options.loop) index = 0;
-      else return;
-    }
-
-    if (typeof this.options.onLeave === "function") {
-      this.options.onLeave(this.index, index);
-    }
-
-    this.index = index;
-    this.isAnimating = true;
-
-    this.container.style.transform = `translateY(-${this.index * 100}vh)`;
+  scrollToSection(index) {
+    this.isScrolling = true;
+    window.scrollTo({
+      top: this.sections[index].offsetTop,
+      behavior: "smooth"
+    });
 
     setTimeout(() => {
-      this.isAnimating = false;
-      if (typeof this.options.afterLoad === "function") {
-        this.options.afterLoad(this.index);
-      }
-    }, this.options.speed);
-  }
-
-  next() {
-    this.goTo(this.index + 1);
-  }
-
-  prev() {
-    this.goTo(this.index - 1);
+      this.isScrolling = false;
+    }, this.options.scrollSpeed);
   }
 }
-
-window.MyFullPage = MyFullPage;
